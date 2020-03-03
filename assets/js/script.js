3c71bd98fbeafcd98258a7e895ba6415afe502f5
iamtheasad <iamtheasad@gmail.com>
2020-03-03 19:29:31 +0600

    home page 1 full added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,54 @@
-(function iamStrict(){
-   "use strict"; // Javascript Strict Method
+(function iamStrict() {
+    "use strict"; // Javascript Strict Method
 
-   jQuery(document).ready(function () {
+    jQuery(document).ready(function () {
 
-      // Place All Code Here
-      
-   });
+        /* Case Study Slider Start */
+        $('.case_studies_carousel').owlCarousel({
+            loop: true,
+            margin: 30,
+            nav: false,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3
+                }
+            }
+        })
+        /* Case Study Slider Start */
 
- })();
- 
+
+        /* Testimonial Slider Start */
+        $(".testimonial_carousel").owlCarousel({
+            items: 1,
+            loop: true,
+            margin: 0,
+            nav: true,
+            autoplay: true,
+            autoplaySpeed: 1000,
+        });
+        /* Testimonial Slider End */
+
+        /* Home Page Company Logo Carousel Start */
+        $('.logo_carousel').owlCarousel({
+            items: 5,
+            loop: true,
+            margin: 100,
+            autoplay: true,
+            autoplaySpeed: 1000,
+            nav: false,
+            dots: false,
+        });
+        /* Home Page Company Logo Carousel End */
+
+    });
+
+})();
